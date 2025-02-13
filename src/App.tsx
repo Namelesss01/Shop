fffa680aafa466841d2805344b22beae164f5d2b
Namelesss01 <zajloobekovali00@gmail.com>
2025-02-13 16:56:22 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+function App() {
+  return (
+    <div className="min-h-[100vh] mx-auto">
+      <Button>12345678</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import { HOME_ITEM } from "./const";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <Button>wqe</Button>
+    </div>
+  );
+};
+
+export default Home;
