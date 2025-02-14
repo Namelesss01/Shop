cb5e72c66a0dcb78655ae12b4ff1351a9de0a126
Kostenko Oleg <ok609035@gmail.com>
2025-02-15 02:15:50 +0600

    create favorites page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import Favorites from "./pages/favorites/Favorites";
 function App() {
   return (
     <div className="min-h-[100vh] mx-auto">
-      <Button>12345678</Button>
+      <Routes>
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
     </div>
   );
 }
