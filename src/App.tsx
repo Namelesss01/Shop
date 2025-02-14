734e3caae55a3f459ea40c4ed35740427f76d30b
Kostenko Oleg <ok609035@gmail.com>
2025-02-15 04:03:45 +0600

    create more page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Favorites from "./pages/favorites/Favorites";
+import More from "./pages/more/More";
 function App() {
   return (
     <div className="min-h-[100vh] mx-auto">
       <Routes>
         <Route path="/favorites" element={<Favorites />} />
+        <Route path="/more" element={<More />} />
       </Routes>
     </div>
   );
