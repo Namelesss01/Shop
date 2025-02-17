b10d7e672d8d4bcf59b87d5c0e5023ef85fe2ab7
Namelesss01 <zajloobekovali00@gmail.com>
2025-02-18 01:19:43 +0600

    21

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Button } from "./components/ui/button";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Basket from "./pages/busket/Basket";
+import Footer from "./shared/footer/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="flex items-center justify-center">
-      <Home />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
