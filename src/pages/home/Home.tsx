a537bfb58e9739b21f9c17ab3efd721e988590bd
Kostenko Oleg <ok609035@gmail.com>
2025-03-09 02:15:38 +0600

    add search product and footer navigation

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import post from "../../assets/img/group7.jpg";
 import { Search } from "lucide-react";
 import { Input } from "../../components/ui/input";
 import Product from "../../shared/product/Product";
 import Footer from "../../shared/footer/Footer";
 import { Auth } from "../../shared/authorization/Auth";
- 
+
 const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("Все");
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  // Функция обработки ввода текста в поле поиска
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value); // Обновляем состояние поиска
+  };
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -20,10 +26,11 @@ const Home = () => {
             <Input
               placeholder="Поиск товаров"
               className="w-full rounded-3xl pl-10 md:pl-20" // изменим отступ в зависимости от разрешения
+              value={searchQuery} // Значение для поля поиска
+              onChange={handleSearch} // Функция для обновления searchQuery
             />
           </div>
-          <Product selectedCategory={selectedCategory} />{" "}
-          {/* Передаем категорию */}
+          <Product selectedCategory={selectedCategory} searchQuery={searchQuery} /> {/* Передаем searchQuery */}
         </div>
       </div>
       <div className="mt-20"></div>
